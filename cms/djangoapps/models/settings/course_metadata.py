14a5534ad2d9ffa8c259f8ad030743ac7d43b531
Alex Dusenbery <adusenbery@edx.org>
2017-06-23 11:10:27 -0400

    Check that both switches are disabled before filtering.

--- cms/djangoapps/models/settings/course_metadata.py
+++ cms/djangoapps/models/settings/course_metadata.py
@@ -106,8 +106,8 @@ class CourseMetadata(object):
         # TODO: Alex Dusenbery 2017-06-22
         # Before we roll out the auto-certs feature, move this to a good, shared
         # place such that we're not repeating code found in LMS.
-        certificate_switches = WaffleSwitchNamespace(name=u'certificates', log_prefix=u'Certificates: ')
-        if not certificate_switches.is_enabled(u'self_paced_only'):
+        switches = WaffleSwitchNamespace(name=u'certificates', log_prefix=u'Certificates: ')
+        if not (switches.is_enabled(u'self_paced_only') or switches.is_enabled(u'instructor_paced_only')):
             filtered_list.append('certificate_available_date')
 
         return filtered_list
